3326be911744f8a35815a5c70bff904c75895929
vlad_rodionov <varodionov70@gmail.com>
2022-08-24 20:08:41 +0700

    LeetCode 2 exercise

---
+++ medium/75. Sort Colors.py
@@ -0,0 +1,17 @@
+def sort_colors(nums):
+
+    mid = 1
+    i = 0
+    j = 0
+    k = len(nums) - 1
+
+    while j <= k:
+        if nums[j] < mid:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j += 1
+        elif nums[j] > mid:
+            nums[j], nums[k] = nums[k], nums[j]
+            k -= 1
+        else:
+            j += 1
